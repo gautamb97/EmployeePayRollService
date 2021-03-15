3983e5ced382168b41e805a2e2c24d51b7f5cf50
gautamb97 <www.gautam971997@gmail.com>
2021-03-15 15:53:25 +0530

    Added:Created employee payroll service to read and writeto a console

--- main/java/employeepayroll/EmployeePayrollService.java
+++ main/java/employeepayroll/EmployeePayrollService.java
@@ -1,5 +1,38 @@
 package employeepayroll;
 
+import java.util.*;
+
 public class EmployeePayrollService {
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+    private List<EmployeePayroll> employeePayrollList;
+
+    public EmployeePayrollService(){
+
+    }
+
+    public EmployeePayrollService(List<EmployeePayroll>employeePayrollList){
+        this.employeePayrollList = employeePayrollList;
+    }
+    private void writeEmployeePayrollData(){
+        System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+    }
+
+    private void readEmployeePayrollData(Scanner consoleInputReader){
+        System.out.println("Enter Employee ID: ");
+        int employeeId = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String employeeName = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double employeeSalary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayroll(employeeId, employeeName, employeeSalary));
+    }
+
+    public static void main (String[] args){
+        ArrayList<EmployeePayroll> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
 
 }
