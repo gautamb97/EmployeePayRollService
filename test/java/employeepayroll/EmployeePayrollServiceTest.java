43ed0f5a8469b010e6b0f0aec71b34293471b3ee
gautamb97 <www.gautam971997@gmail.com>
2021-03-19 20:19:46 +0530

    Modified:Added multiple employee to a payroll DB using thread

--- test/java/employeepayroll/EmployeePayrollServiceTest.java
+++ test/java/employeepayroll/EmployeePayrollServiceTest.java
@@ -115,6 +115,10 @@ public class EmployeePayrollServiceTest {
         employeePayrollService.addEmployeesToPayroll(Arrays.asList(arrayOfEmps));
         Instant end = Instant.now();
         System.out.println("Duration without thread: "+ Duration.between(start, end));
-        Assert.assertEquals(7, employeePayrollService.countEntries(DB_IO));
+        Instant threadStart = Instant.now();
+        employeePayrollService.addEmployeesToPayrollWithThreads(Arrays.asList(arrayOfEmps));
+        Instant threadEnd = Instant.now();
+        System.out.println("Duration with Thread: "+Duration.between(threadStart, threadEnd));
+        Assert.assertEquals(12, employeePayrollService.countEntries(DB_IO));
     }
 }
